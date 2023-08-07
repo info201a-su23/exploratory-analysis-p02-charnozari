9e33f3316e5e7a74d9957f8eab246a1b75c374f9
unknown <jspeel@uw.edu>
2023-08-06 19:15:39 -0700

    Chart 1 is now completed

--- chart1_example.R
+++ chart1_example.R
@@ -1,7 +1,26 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
 library(ggplot2)
-ggplot() +
-  geom_point(aes(x=x_values, y = y_values))
+library(dplyr)
+library(lubridate)
 
+major_city_df <- 
+  read.csv("GlobalLandTemperaturesByMajorCity.csv")
+
+# only data from China from 1990-2014
+china_temps <- major_city_df %>%
+  filter(Country == "China", dt >= 1990 & dt <= 2014)
+
+# separate the year from the rest of the date
+china_temps$dt <- year(china_temps$dt)
+
+# find average temp for each city during every year
+avg_temps_city <- china_temps %>%
+  group_by(dt, City) %>%
+  summarize(AverageTemperature = mean(AverageTemperature, na.rm = TRUE))
+
+ggplot(avg_temps_city, aes(x = dt, y = AverageTemperature, color = City)) +
+  geom_line() +
+  labs(title = "Average Temperature of Major Cities in China, 1990-2013",
+       x = "Year",
+       y = "Average Temperature (in degrees Celsius)",
+       color = "City") +
+  theme_classic()
