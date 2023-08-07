0e0af92aa23dc2d0e68c413b12b2b156287cc1d4
reollei <leila.peitsch@gmail.com>
2023-08-06 18:28:28 -0700

    commented map_example.R

--- map_example.R
+++ map_example.R
@@ -1,20 +1,25 @@
+# Load packages
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(maps)
 
+# Load data (only pathway for now)
 temp_city <- read.csv('../../data/GlobalLandTemperaturesByCity.csv')
 world <- map_data("world")
 
-length(unique(temp_city$Country))
-
+# Clean data
 temp_city_clean <- temp_city %>% 
   filter(dt > "2000-01-01") %>% 
-  distinct(Country, .keep_all = TRUE) %>% 
   select(region = Country, temp = AverageTemperature)
 
+# Average temperatures per country
+temp_city_clean <- aggregate(temp ~ region, data = temp_city_clean, FUN = "mean") 
+
+# Join world data and temp. data
 world_sub <- inner_join(world, temp_city_clean, by = "region")
 
+# Formatting map
 plain <- theme(
   axis.text = element_blank(),
   axis.line = element_blank(),
@@ -23,12 +28,14 @@ plain <- theme(
   panel.grid = element_blank(),
   axis.title = element_blank(),
   panel.background = element_rect(fill = "white"),
-  plot.title = element_text(hjust = 0.5)
+  plot.title = element_text(hjust = 0.5),
+  legend.title.align = 0.5
 )
 
+# Plot map
 worldtemp <- ggplot(data = world_sub, mapping = aes(x = long, y = lat, group = group)) + 
   coord_fixed(1.3) +
   geom_polygon(aes(fill = temp)) +
-  scale_fill_distiller(palette ="RdBu", direction = -1) + # or direction=1
-  ggtitle("Global Temperature") +
+  scale_fill_distiller(name = "Avg. Temp in °C", palette ="RdBu", direction = -1) + # or direction=1
+  ggtitle("Global Temperatures") +
   plain
