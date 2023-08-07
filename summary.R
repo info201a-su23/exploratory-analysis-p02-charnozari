22153f2f4baa84115d49d2d1d5e7d7c3bcb1beab
Humail-Fatima <humail.fatima2002@gmail.com>
2023-08-07 01:00:57 -0700

    Summary added!

---
+++ summary.R
@@ -0,0 +1,46 @@
+library(dplyr)
+
+Cdataset <- read.csv("C:/Users/charl/info201/exploratory-analysis-p02-charnozari/GlobalLandTemperaturesByCity.csv")
+Cdataset$dt <- as.Date(Cdataset$dt)
+data_1990_US <- Cdataset %>%
+  filter(dt >= as.Date("1990-01-01") & dt <= as.Date("1990-12-31") & Country == "United States")
+average_temp_US_1990 <- mean(data_1990_US$AverageTemperature, na.rm = TRUE)
+
+average_temp_us_1990_function <- function(){
+  i <- average_temp_US_1990
+  return(i)
+}
+
+data_1990_CN <- Cdataset %>%
+  filter(dt >= as.Date("1990-01-01") & dt <= as.Date("1990-12-31") & Country == "China")
+average_temp_CN_1990 <- mean(data_1990_CN$AverageTemperature, na.rm = TRUE)
+
+average_temp_CN_1990_function <- function(){
+  i <- average_temp_CN_1990
+  return(i)
+}
+
+data_2013_US <- Cdataset %>%
+  filter(dt >= as.Date("2013-01-01") & dt <= as.Date("2013-12-31") & Country == "United States")
+average_temp_US_2013 <- mean(data_2013_US$AverageTemperature, na.rm = TRUE)
+
+average_temp_us_2013_function <- function(){
+  i <- average_temp_US_2013
+  return(i)
+}
+
+data_2013_CN <- Cdataset %>%
+  filter(dt >= as.Date("2013-01-01") & dt <= as.Date("2013-12-31") & Country == "China")
+average_temp_CN_2013 <- mean(data_2013_CN$AverageTemperature, na.rm = TRUE)
+
+average_temp_CN_2013_function <- function(){
+  i <- average_temp_CN_2013
+  return(i)
+}
+
+average_temp_diff_US <- average_temp_US_2013 - average_temp_US_1990
+
+average_temp_diff_US_function <- function(){
+  i <- average_temp_diff_US
+  return(i)
+}
